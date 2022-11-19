3e0ee196d0a67e0b051bf07799fab19c3deb9f45
wujuno <86146780+wujuno@users.noreply.github.com>
2022-11-19 14:01:41 +0900

    Header part One

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,28 @@
-import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Header from "./Components/Header";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
 
-
-function App() {
+function App() {	
   return (
-    <div>
-    </div>
-  );
-}
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="/search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
+    )
+  
+}	
+
 
 export default App;
